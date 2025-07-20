d9245720bab26a2404735cccfbadd9ae36b0571f
Tomas Ramirez <tiramirez@uc.cl>
2025-07-20 13:12:32 -0400

    [fix] bucket remove acl

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "${var.bucket_name}-${terraform.workspace}"
-  acl    = "private"
 
   versioning = {
     enabled = true
